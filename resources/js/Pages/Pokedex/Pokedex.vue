f4fb7b63be94eb1e817301fc59f2b3aee7f49738
AmauryGuillin <amauryg@hotmail.fr>
2024-10-19 16:56:36 +0200

    feat: upgrade search function on pokedex pahe :sparkles:

--- resources/js/Pages/Pokedex/Pokedex.vue
+++ resources/js/Pages/Pokedex/Pokedex.vue
@@ -6,15 +6,31 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head } from "@inertiajs/vue3";
 import { Search } from "lucide-vue-next";
 import { ref } from "vue";
+import {createLogger} from "vite";
 
 const props = defineProps<{
     pokemons: any;
     types: any;
 }>();
 
+const pokemonArray = ref(props.pokemons);
+
 const searchWord = ref("");
+const searchType = ref<null | string>("");
 function searchPokemon(input: string) {
     searchWord.value = input.toLowerCase().trim();
+    pokemonArray.value = props.pokemons.filter((p: any) => p.name.toLowerCase().trim().startsWith(searchWord.value));
+}
+
+function searchByType(input: string) {
+    if(!input) return;
+    searchType.value = input === searchType.value ? null : input;
+    if (!searchType.value) {
+        pokemonArray.value = props.pokemons
+        return;
+    }
+    pokemonArray.value = props.pokemons.filter((p: any) => p.type_prime.name.toLowerCase().trim() === searchType.value.toLowerCase().trim()
+        || p.type_second?.name.toLowerCase().trim() === searchType.value.toLowerCase().trim())
 }
 </script>
 
@@ -44,20 +60,26 @@ function searchPokemon(input: string) {
                     <div v-for="type in types">
                         <Badge
                             class="cursor-pointer hover:scale-[1.05] transition-all"
-                            :style="{ backgroundColor: type.color.value }"
+                            :class="{'border-2 border-red-500' : searchType === type.name }"
+                            :style="{ backgroundColor: searchType === type.name ? 'gray' : type.color.value }"
+                            @click="searchByType(type.name)"
                             >{{ type.name }}</Badge
                         >
                     </div>
                 </div>
             </div>
             <div
+                v-if="pokemonArray.length !== 0"
                 class="grid justify-center grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mx-2 lg:mx-48"
             >
                 <PokemonCardSimple
-                    v-for="pokemon in pokemons.filter((p :any) => p.name.toLowerCase().trim().includes(searchWord))"
+                    v-for="pokemon in pokemonArray"
                     :key="pokemon.name"
                     :pokemon="pokemon"
                 />
+            </div>
+            <div v-else class="text-center font-bold p-48">
+                <span>Aucun Pokémon trouvé.</span>
             </div>
         </div>
     </AuthenticatedLayout>
